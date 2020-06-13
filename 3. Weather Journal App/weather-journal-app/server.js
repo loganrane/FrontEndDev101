e6f857e0189930ccf72b201128d7837379cd84b8
Arpit <arpitfalcon1@gmail.com>
2020-06-13 19:04:30 +0530

    feat: made the first draft of server side js

--- 3. Weather Journal App/weather-journal-app/server.js
+++ 3. Weather Journal App/weather-journal-app/server.js
@@ -2,18 +2,43 @@
 projectData = {};
 
 // Require Express to run server and routes
+let express = require("express");
 
 // Start up an instance of app
+let app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+let bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+let cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+const port = 8000;
+
+const server = app.listen(port, function () {
+    console.log("Server is running.");
+    console.log(`Server is at localhost : ${port}`);
+});
+
+// GET Route
+app.get("/all", sendData);
+
+function sendData(req, res) {
+    res.send(projectData);
+}
+
+// POST Route
+const data = [];
+app.post("/add", callBack);
+
+function callBack(req, res) {
+    data.push(req.body);
+}
--- 3. Weather Journal App/weather-journal-app/website/app.js
+++ 3. Weather Journal App/weather-journal-app/website/app.js
@@ -1,5 +1,11 @@
 /* Global Variables */
-
+const apiKey = "176b2eca49b0489b01225ef0b8981701";
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+document.getElementById("generate").addEventListener("click", generateFunction);
+
+function generateFunction() {
+    const zipCode = document.getElementById("zip").value;
+}
