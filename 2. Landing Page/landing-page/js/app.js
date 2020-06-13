799e9068bca1696104be567844768d2959ec5ab3
Arpit <arpitfalcon1@gmail.com>
2020-06-13 08:56:59 +0530

    feat: added the scroll to change active class

--- 2. Landing Page/landing-page/js/app.js
+++ 2. Landing Page/landing-page/js/app.js
@@ -17,7 +17,11 @@
  * Define Global Variables
  *
  */
-
+let headings = document.getElementsByTagName("section");
+let navbarList = document.querySelector("#navbar__list");
+let mainSection = document.querySelectorAll("main section");
+let mainNavLinks = document.querySelector("nav ul li a");
+let heigthOfSection = 0;
 /**
  * End Global Variables
  * Start Helper Functions
@@ -31,20 +35,35 @@
  */
 
 // build the nav
-let headings = document.getElementsByTagName("section");
-let navbarList = document.querySelector("#navbar__list");
+
 for (let temp of headings) {
     let list = document.createElement("li");
-    list.setAttribute("class", "menu__link");
     let anchorTag = document.createElement("a");
+
     anchorTag.textContent = temp.dataset.nav;
     anchorTag.setAttribute("href", "#" + temp.id);
-    list.appendChild(anchorTag);
-    // let appendToNavbar;
+    anchorTag.setAttribute("class", "menu__link");
 
+    list.appendChild(anchorTag);
     navbarList.insertAdjacentElement("beforeend", list);
 }
+
 // Add class 'active' to section when near top of viewport
+
+let scrollPos = window.scrollY;
+const addClassOnScroll = function () {};
+
+window.addEventListener("scroll", function () {
+    scrollPos = window.scrollY;
+    heigthOfSection = mainSection[0].offsetHeight;
+
+    for (let i = 0; i < mainSection.length; i++) {
+        mainSection[i].classList.remove("your-active-class");
+    }
+
+    let addActiveTo = Math.floor(scrollPos / heigthOfSection);
+    mainSection[addActiveTo].classList.add("your-active-class");
+});
 
 // Scroll to anchor ID using scrollTO event
 
