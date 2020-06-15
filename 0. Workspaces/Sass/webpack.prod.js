284d958364cfdfec470984ad1f8320266c15b510
Arpit <arpitfalcon1@gmail.com>
2020-06-15 12:29:33 +0530

    feat: add minified css

--- 0. Workspaces/Sass/webpack.prod.js
+++ 0. Workspaces/Sass/webpack.prod.js
@@ -1,20 +1,26 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssestsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: "./src/client/index.js",
     mode: "production",
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssestsPlugin({})],
+    },
     module: {
         rules: [
             {
-                test: "/.js$/",
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader",
             },
             {
-                test: "/.scss$/",
-                use: ["style-loader", "css-loader", "sass-loader"],
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
         ],
     },
@@ -23,5 +29,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
     ],
 };
