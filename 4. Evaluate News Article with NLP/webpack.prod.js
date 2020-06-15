80dd878048bb15e8a5810086f980f409c9b455ea
Arpit <arpitfalcon1@gmail.com>
2020-06-15 17:48:55 +0530

    feat: complete the config of production side webpack

--- 4. Evaluate News Article with NLP/webpack.prod.js
+++ 4. Evaluate News Article with NLP/webpack.prod.js
@@ -1,16 +1,27 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssestsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+// const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
     entry: "./src/client/index.js",
     mode: "production",
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssestsPlugin({})],
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader",
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
         ],
     },
@@ -19,5 +30,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        // new WorkboxPlugin.GenerateSW(),
     ],
 };
