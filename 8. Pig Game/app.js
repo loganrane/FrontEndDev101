2d6db2270edb7e981f46f5a518c0e08741c1844b
arpitfalcon <arpitfalcon1@gmail.com>
2020-06-30 19:02:44 +0530

    feat: Add the hold  button function

--- 8. Pig Game/app.js
+++ 8. Pig Game/app.js
@@ -37,14 +37,42 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	} else {
 		// Next Player and all scores are lost
-		activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-		roundScore = 0;
-		document.getElementById('current-0').textContent = '0';
-		document.getElementById('current-1').textContent = '0';
-
-		document.querySelector('.player-0-panel').classList.toggle('active');
-		document.querySelector('.player-1-panel').classList.toggle('active');
-
-		document.querySelector('.dice').style.display = 'none';
+		nextPlayer();
 	}
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+	// 1. Add the current score to user's global score
+	scores[activePlayer] += roundScore;
+
+	// 2. Update the UI
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	// 3. Check if player won the game
+	if (scores[activePlayer] >= 100) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+		document.querySelector('.dice').style.display = 'none';
+		document
+			.querySelector('.player-' + activePlayer + '-panel')
+			.classList.add('winner');
+		document
+			.querySelector('.player-' + activePlayer + '-panel')
+			.classList.remove('active');
+	} else {
+		// 4. Switch Player
+		nextPlayer();
+	}
+});
+
+// Switch to the next player
+function nextPlayer() {
+	activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+	roundScore = 0;
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+
+	document.querySelector('.dice').style.display = 'none';
+}
