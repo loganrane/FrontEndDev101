20b6804411fd89fa54296bd061318a1b298de736
arpitfalcon <arpitfalcon1@gmail.com>
2020-06-30 18:14:27 +0530

    feat: Add the features in roll dice button

--- 8. Pig Game/app.js
+++ 8. Pig Game/app.js
@@ -22,13 +22,29 @@ document.getElementById('current-0').textContent = 0;
 document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
-  // 1. Random number
-  var dice = Math.floor(Math.random() * 6) + 1;
+	// 1. Random number
+	var dice = Math.floor(Math.random() * 6) + 1;
 
-  // 2. Display the results
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = './dice-' + dice + '.png';
+	// 2. Display the results
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = './dice-' + dice + '.png';
 
-  // 3. Update the round score IF the rolled number was NOT a 1.
+	// 3. Update the round score IF the rolled number was NOT a 1.
+	if (dice !== 1) {
+		// Add score to the round score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		// Next Player and all scores are lost
+		activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+		roundScore = 0;
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		document.querySelector('.dice').style.display = 'none';
+	}
 });
